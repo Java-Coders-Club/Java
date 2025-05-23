716ad12e6832de69f353bb09f3951c826b932482
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-05-23 22:18:57 +0530

    Scope of a Variable

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Demo
+{
+	static String name = "java"; // static global variable
+	double Salary; //global variable non static storing default vale 0.0
+
+	public static void main(String[] args)
+	{
+		int age=12; // local variable 
+
+	}
+}
